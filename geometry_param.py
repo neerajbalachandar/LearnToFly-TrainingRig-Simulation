ee2dff252a0b6de257d3763f5473a7691467e851
YashwanthProfile <yashwanth.m1ga16me123@gmail.com>
2026-06-11 16:43:29 +0530

    chore: clearned notebook outputs and gitignore

--- geometry_param.py
+++ geometry_param.py
@@ -1,37 +1,26 @@
-import numpy as np
+def get_quad_params():
+    return {
+        'model_name': 'LiteWing',
+        'spec_source_url': 'https://circuitdigest.com/wiki/litewing/',
+        'frame_span_m': 0.1,
+        'quad_l': 0.05,
+    }
 
 def get_params():
-#     return {
-#         'g': 9.81,
-#         'ma': 1.0,
-#         'mb': 1.2,
-#         'mc': 0.8,
-
-#         'Iaxx': 0.02, 'Iayy': 0.02, 'Iazz': 0.02,
-#         'Iaxy': 0.0,  'Iaxz': 0.0,  'Iayz': 0.0,
-
-#         'Ibx': 0.01, 'Iby': 0.01, 'Ibz': 0.01,
-#         'Icx': 0.01, 'Icy': 0.01, 'Icz': 0.01,
-
-#         'xO_A': 0.0, 'yO_A': 0.0, 'zO_A': 0.0,
-#         'xB_A': 0.1, 'yB_A': 0.0, 'zB_A': 0.0,
-#         'xC_A': -0.1,'yC_A': 0.0, 'zC_A': 0.0,
-# }
-
     return {
         'g': 9.81,
-        'ma': 2,
-        'mb': 2.2,
-        'mc': 1.3,
-        'Iaxx': 0.00734182940332,
-        'Iayy': 0.0410836362018,
-        'Iazz': 0.0337428067985,
+        'ma': 0.5,
+        'mb': 0.045,
+        'mc': 0.06,
+        'Iaxx': 0.00183545735083,
+        'Iayy': 0.0102709090504,
+        'Iazz': 0.00843570169962,
         'Iaxy': 0,
-        'Iaxz': 0.0109484282428,
+        'Iaxz': 0.0027371070607,
         'Iayz': 0,
-        'Ibx': 0.03,
-        'Iby': 0.025,
-        'Ibz': 0.02,
+        'Ibx': 5.625e-05,
+        'Iby': 5.625e-05,
+        'Ibz': 0.0001125,
         'Icx': 0.03,
         'Icy': 0.025,
         'Icz': 0.02,
